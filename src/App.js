9ce0dbbafe4916e2efcc2537a140e385d8e90f56
JeandeAndradeNeves <jeandeneves@outlook.com>
2022-09-20 15:45:10 -0300

    Arquitetura do projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Globalstyle from "./styles/global";
+
+const App = () => {
+  return (
+    <>
+       <Header/>      
+       <Globalstyle/>
+    </>
+  );
+};
+
+export default App;
