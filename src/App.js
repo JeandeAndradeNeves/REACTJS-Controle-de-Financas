cdbaa311ff8c0d6b0ca76b35442cdb03165104e0
JeandeAndradeNeves <jeandeneves@outlook.com>
2022-09-21 15:53:25 -0300

    Criação dos componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Resume from "./components/Resume";
 import Globalstyle from "./styles/global";
 
 const App = () => {
   return (
     <>
-       <Header/>      
+       <Header/> 
+       <Resume/>     
        <Globalstyle/>
     </>
   );
---
+++ src/components/ResumeItem/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import * as C from "./styles";
+
+const ResumeItem = () => {
+  return (<div>amor</div>
+  );
+};
+
+export default ResumeItem;
