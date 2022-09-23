e4f474a5784c4c059f18e2f05ac1ca1ac82c4194
Jean Andrade <jeandeneves@outlook.com>
2022-09-23 15:26:02 -0300

    Componente form e estilizações

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Resume from "./components/Resume";
 import Globalstyle from "./styles/global";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <>
        <Header/> 
-       <Resume/>     
+       <Resume/>
+       <Form/>     
        <Globalstyle/>
     </>
   );
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ResumeItem from "../ResumeItem";
 import * as C from "./styles";
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from "react-icons/fa";
 
 const Resume = () => {
   return ( 
   <C.Container>
-    <ResumeItem/>
-    <ResumeItem/>
-    <ResumeItem/>
+    <ResumeItem title="Entradas" Icon={FaRegArrowAltCircleUp} value={100}/>
+    <ResumeItem title="Saídas" Icon={FaRegArrowAltCircleDown} value={100}/>
+    <ResumeItem title="Total" Icon={FaDollarSign} value={100}/>
   </C.Container>
   )
 };
