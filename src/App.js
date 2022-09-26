01283962613b344a5388d4215fda4f061eb2a52f
Jean Andrade <jeandeneves@outlook.com>
2022-09-26 15:18:30 -0300

    App - useEffect pegando entrada e saida.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import Globalstyle from "./styles/global";
 import Form from "./components/Form";
 
 const App = () => {
+  const data = localStorage.getItem("transactions");
+  const [transactionsList, setTransactionsList] = useState(
+    data ? JSON.parse(data) : []
+  );
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    //pegando a saida
+    const amountExpense = transactionsList
+      .filter((item) => item.expense)
+      .map((transaction) => Number(transaction.amount));
+    //pegando a entrada
+    const amountIncome = transactionsList
+      .filter((item) => !item.expense)
+      .map((transaction) => Number(transaction.amount));
+  
+}, [transactionsList]);
+
+
   return (
     <>
        <Header/> 
